40e2e7878d47b166e879b72fed88155b8d7857b7
HwangBBang <h1009218@hufs.ac.kr>
2024-08-28 03:17:42 +0900

    [feat] implement common error

---
+++ service/src/main/java/com/its/service/common/error/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.its.service.common.error.exception;
+
+import com.its.service.common.error.code.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
