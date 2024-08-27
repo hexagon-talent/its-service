5dc33206f6f6baacc9e5002338af473a65c1e0bd
HwangBBang <h1009218@hufs.ac.kr>
2024-08-28 03:22:18 +0900

    [feat] implement User Entity

---
+++ service/src/main/java/com/its/service/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.its.service.domain.repository;
+
+import com.its.service.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
