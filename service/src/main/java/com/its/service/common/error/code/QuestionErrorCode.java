504bfd7e8dfe1a4be0e637812116b67efbec85d1
HwangBBang <h1009218@hufs.ac.kr>
2024-09-08 15:16:07 +0900

    [feat] Error code enum 구현

---
+++ service/src/main/java/com/its/service/common/error/code/QuestionErrorCode.java
@@ -0,0 +1,16 @@
+package com.its.service.common.error.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum QuestionErrorCode implements ErrorCode{
+    QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "QUESTION-001", "해당 문제는 존재하지 않습니다."),
+    QUESTION_ACCESS_DENIED(HttpStatus.FORBIDDEN, "QUESTION-002", "해당 문제에 접근 권한이 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
---
+++ service/src/main/java/com/its/service/common/error/code/UserErrorCode.java
@@ -0,0 +1,15 @@
+package com.its.service.common.error.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UserErrorCode implements ErrorCode  {
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-001", "해당 사용자는 존재하지 않습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
