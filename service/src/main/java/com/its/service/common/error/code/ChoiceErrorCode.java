68fe4540af077b10b142cb2f488be84d0081d0bc
HwangBBang <h1009218@hufs.ac.kr>
2024-09-10 14:15:40 +0900

    [feat] Add error codes (Enum) for each domain

--- service/src/main/java/com/its/service/common/error/code/ChoiceErrorCode.java
+++ service/src/main/java/com/its/service/common/error/code/ChoiceErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ChoiceErrorCode implements ErrorCode{
-    CHOICE_NOT_FOUND(HttpStatus.NOT_FOUND, "CHOICE-001", "해당 선지는 존재하지 않습니다.");
+    CHOICE_NOT_FOUND(HttpStatus.NOT_FOUND, "CHOICE-001", "해당 선지는 존재하지 않습니다."),
+    CHOICE_ALREADY_EXISTS(HttpStatus.CONFLICT, "CHOICE-002", "해당 선지는 이미 존재합니다."),
+    CHOICE_ACCESS_DENIED(HttpStatus.FORBIDDEN, "CHOICE-003", "해당 선지에 접근 권한이 없습니다.");
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
--- service/src/main/java/com/its/service/common/error/code/UserErrorCode.java
+++ service/src/main/java/com/its/service/common/error/code/UserErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode  {
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-001", "해당 사용자는 존재하지 않습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-001", "해당 사용자는 존재하지 않습니다."),
+    USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER-002", "해당 사용자는 이미 존재 합니다.");
+
 
     private final HttpStatus httpStatus;
     private final String code;
