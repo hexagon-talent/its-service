c3d4c9eeeac00186cef4c67aca4074f3930e3fb4
HwangBBang <h1009218@hufs.ac.kr>
2024-11-17 16:18:25 +0900

    refactor: Delegate domain-specific query logic to individual query services

--- service/src/main/java/com/its/service/domain/question/service/choice/ChoiceQueryService.java
+++ service/src/main/java/com/its/service/domain/question/service/choice/ChoiceQueryService.java
@@ -1,11 +1,9 @@
 package com.its.service.domain.question.service.choice;
 
-import com.its.service.common.error.code.QuestionErrorCode;
-import com.its.service.common.error.exception.CustomException;
 import com.its.service.domain.question.dto.response.ChoicesResponse;
 import com.its.service.domain.question.entity.Question;
 import com.its.service.domain.question.mapper.ChoiceMapper;
-import com.its.service.domain.question.repository.QuestionRepository;
+import com.its.service.domain.question.service.question.QuestionQueryService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,13 +14,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class ChoiceQueryService {
-    private final QuestionRepository questionRepository;
     private final ChoiceMapper choiceMapper;
+    private final QuestionQueryService questionQueryService;
+
 
     @Transactional
     public ChoicesResponse getChoicesByQuestionId(String questionId) {
-        Question question = questionRepository.findById(questionId)
-                .orElseThrow(() -> new CustomException(QuestionErrorCode.QUESTION_NOT_FOUND));
+        Question question = questionQueryService.findQuestionById(questionId);
         return choiceMapper.toChoicesResponse(question);
     }
+
 }
--- service/src/main/java/com/its/service/domain/question/service/explanation/ExplanationQueryService.java
+++ service/src/main/java/com/its/service/domain/question/service/explanation/ExplanationQueryService.java
@@ -6,6 +6,7 @@ import com.its.service.domain.question.dto.response.ExplanationResponse;
 import com.its.service.domain.question.entity.Question;
 import com.its.service.domain.question.mapper.ExplanationMapper;
 import com.its.service.domain.question.repository.QuestionRepository;
+import com.its.service.domain.question.service.question.QuestionQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,13 +16,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ExplanationQueryService {
     private final ExplanationMapper explanationMapper;
-    private final QuestionRepository questionRepository;
+    private final QuestionQueryService questionQueryService;
     /*
      * 특정 문제의 해설 조회
      * */
     public ExplanationResponse getExplanationByQuestionId(String questionId) {
-        Question question = questionRepository.findById(questionId)
-                .orElseThrow(() -> new CustomException(QuestionErrorCode.QUESTION_NOT_FOUND));
+        Question question = questionQueryService.findQuestionById(questionId);
 
         return explanationMapper.toExplanationResponse(question);
     }
